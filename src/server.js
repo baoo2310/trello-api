701b9d0dc2e619539b73daff3190139f48a0b81a
baoo2310 <baoo231005@gmail.com>
2026-02-05 14:18:20 +0700

    init

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+import express from 'express';
+
+const app = express();
+const hostname = 'localhost';
+const port = 8017;
+
+app.get('/', function (req, res)  {
+    res.send('<h1>Hello World</h1>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server is runninng at port http://${hostname}:${port}/`);
+})
