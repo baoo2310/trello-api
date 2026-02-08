d58c7091e3c09b5a674913b8c784d991519b79e9
baoo2310 <baoo231005@gmail.com>
2026-02-08 20:41:54 +0700

    Build & product env and fix import error

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -1,4 +1,5 @@
 import { StatusCodes } from "http-status-codes";
+import { env } from "../config/environment.js";
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
     // Always default code 500 if dev forget the status code
@@ -8,5 +9,8 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
         message: err.message || StatusCodes[err.statusCode],
         stack: err.stack
     }
+
+    if (env.BUILD_MODE !== "dev") delete responseError.stack;
+
     res.status(responseError.statusCode).json(responseError);
 }
